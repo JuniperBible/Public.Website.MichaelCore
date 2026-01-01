bf792fd1fbc57be150c481b7ed573e2774c63fcf
cyanitol <gitnoreply@cyanitol.com>
2026-01-01 14:07:04 -0500

    ADD shell.nix for standalone development

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    hugo
+    nodejs_22
+  ];
+
+  shellHook = ''
+    echo "Michael - Bible UI Hugo Module"
+    echo "==============================="
+    echo ""
+    echo "This is a Hugo module, not standalone."
+    echo "Use from parent project with module mounts."
+    echo ""
+    echo "Contents:"
+    echo "  layouts/religion/bibles/  - Bible page layouts"
+    echo "  assets/js/               - Bible JavaScript"
+    echo "  i18n/                    - Translations"
+    echo "  content/bibles/          - Bible content pages"
+    echo ""
+  '';
+}
